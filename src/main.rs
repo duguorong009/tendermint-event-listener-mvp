8887932e380d0bc7aa0a6e7b6eccc01027c179a6
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2022-07-31 00:36:03 -0400

    Implement the first MVP (#1)
    * Initial commit
    
    * Complete the 5-length height queue for querying wasm events
    
    * Colorize some log output
    
    * Define the queue size as const

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::collections::VecDeque;
+use std::str::FromStr;
+
+use tendermint::abci::Event;
+use tendermint::block::Height;
+use tendermint_rpc::Order;
+use tendermint_rpc::{query::Query, Client, HttpClient};
+
+use colored::*;
+
+const LOCAL_RPC: &str = "http://127.0.0.1:26657";
+const JUNO_RPC: &str = "https://rpc-juno.itastakers.com";
+
+const QUEUE_SIZE: usize = 5;
+
+#[tokio::main]
+async fn main() {
+    let mut queue: VecDeque<Height> = VecDeque::new();
+    let client = HttpClient::new(JUNO_RPC).unwrap();
+
+    while let Ok(latest_block) = client.latest_block().await {
+        let latest_block_height = latest_block.block.header.height;
+        match queue.len() {
+            0 => queue.push_back(latest_block_height),
+            1..=QUEUE_SIZE => {
+                let n = queue.len();
+                if latest_block_height != queue[n - 1] {
+                    queue.push_back(latest_block_height);
+                }
+            }
+            _ => {
+                let bh = queue.pop_front().unwrap();
+                show_wasm_events(&client, bh).await;
+            }
+        }
+    }
+    println!("Final: {:?}", queue);
+}
+
+async fn show_wasm_events(client: &HttpClient, height: Height) {
+    println!("Transaction height::: {}", height.value().to_string().green());
+    let searched_tx = client
+        .tx_search(
+            Query::from_str(format!("tx.height = {}", height.value()).as_str()).unwrap(),
+            false,
+            1,
+            100,
+            Order::Ascending,
+        )
+        .await
+        .unwrap();
+
+    if searched_tx.txs.len() > 0 {
+        println!("Searched txs counts: {}", searched_tx.total_count.to_string().red());
+        let tx_count = searched_tx.total_count;
+        for i in 0..tx_count {
+            let tx = &searched_tx.txs[i as usize];
+            let wasm_events = tx
+                .tx_result
+                .events
+                .iter()
+                .filter(|v| v.type_str == *"wasm")
+                .map(|v| v.clone())
+                .collect::<Vec<Event>>();
+            if !wasm_events.is_empty() {
+                println!("Filtered wasm events: {:?}", wasm_events);
+            }
+        }
+    }
+}
